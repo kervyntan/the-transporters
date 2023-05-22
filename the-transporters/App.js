8065854e1492dedf45cf7f07db18da1723b2150f
Kervyn <kervyntan@gmail.com>
2023-05-22 21:37:54 +0800

    Trying animated, backgroundColor prop for StatusBar component

--- the-transporters/App.js
+++ the-transporters/App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar animated backgroundColor='#f9b2c4' />
+      <Text>This is a test application.</Text>
     </View>
   );
 }
